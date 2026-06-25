2dbb1c7c77309bb9f0ef53301a4f1ad51deddabc
Shankar Jangid <shankarjangid550@gmail.com>
2026-06-25 12:10:56 +0530

    Time: 4 ms (23.05%), Space: 12.8 MB (38.56%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -4,6 +4,7 @@ public:
         stack<int> s;
         s.push(-1);
         vector<int> nextG(nums2.size(), -1);
+        unordered_map<int,int> greater;
 
         int n2 = nums2.size();
         for(int i=n2-1; i>=0; i--){
@@ -12,21 +13,15 @@ public:
                 s.pop();
             } 
             nextG[i] = s.top();
+            greater[nums2[i]] = nextG[i];
             s.push(currElem);
         }
 
         vector<int> ans;
         for(int i=0; i<nums1.size(); i++){
-            for(int j=0; j<nums2.size(); j++){
-                if(nums1[i] == nums2[j]){
-                    ans.push_back(nextG[j]);
-                }
-            }
+            ans.push_back(greater[nums1[i]]);
         }
 
-
         return ans;
-
-
     }
 };
