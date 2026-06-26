16ea65f0d7ed7d3b99ed01e785178a7e88a63250
Shankar Jangid <shankarjangid550@gmail.com>
2026-06-26 15:55:03 +0530

    Time: 79 ms (54.3%), Space: 114.8 MB (49.01%) - LeetHub

---
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
+        unordered_map<int, int> mp; // phla wala cumSum dusre wala uski freq ya count
+        mp[0] = 1;
+
+        long long cumSum = 0;
+        long long validLeftPoints = 0;
+        long long ans = 0;
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] == target){
+                validLeftPoints += mp[cumSum];
+                cumSum++;
+
+            }
+            else{
+                cumSum--;
+                validLeftPoints -= mp[cumSum];
+            }
+                ans+= validLeftPoints;
+
+            mp[cumSum]++;
+        }
+
+        return ans;
+    }
+};
