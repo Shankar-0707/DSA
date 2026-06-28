dc091f09925cd3a9c2e377b420e73f6e88039d81
Shankar Jangid <shankarjangid550@gmail.com>
2026-06-28 21:59:11 +0530

    Time: 57 ms (5.29%), Space: 85.8 MB (33.93%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,57 +1,54 @@
 class Solution {
 public:
 
-    void all_next(vector<int>& heights, vector<int>& next){
+    void next(vector<int>& heights, vector<int>& all_next){
         stack<int> s;
         s.push(-1);
         int n = heights.size();
 
         for(int i = n-1; i>=0; i--){
-            int currElement = heights[i];
-
-            while(s.top() != -1 && heights[s.top()] >= currElement){
+            int elem = heights[i];
+            while(s.top() != -1 && heights[s.top()] >= elem){
                 s.pop();
             }
 
-            next.push_back(s.top() == -1 ? n: s.top());
+            all_next[i] = s.top() == -1 ? n : s.top();
             s.push(i);
         }
     }
 
-       void all_prev(vector<int>& heights, vector<int>& prev){
+    void prev(vector<int>& heights, vector<int>& all_prev){
         stack<int> s;
         s.push(-1);
         int n = heights.size();
-
-        for(int i = 0; i<n; i++){
-            int currElement = heights[i];
-
-            while(s.top() != -1 && heights[s.top()] >= currElement){
+        for(int i=0; i<n; i++){
+            int elem = heights[i];
+            while(s.top() != -1 && heights[s.top()] >= elem){
                 s.pop();
             }
 
-            prev.push_back(s.top());
+            all_prev[i] = s.top();
             s.push(i);
         }
     }
 
     int largestRectangleArea(vector<int>& heights) {
-        vector<int> next;
-        vector<int> prev;
-        all_next(heights, next);
-        reverse(next.begin(), next.end());
-        all_prev(heights, prev);
+        int ans = 0;
+        int n = heights.size();
+        vector<int> all_next(n, -1);
+        vector<int> all_prev(n, -1);
 
-        int ans = INT_MIN;
-
-        for(int i =0; i<heights.size(); i++){
-            int width = next[i] - prev[i] - 1;
-            int height = heights[i];
-            int area = width * height;
-            ans = max(ans, area);
+        next(heights, all_next);
+        prev(heights, all_prev);
+        
+        for(int i=0; i<n; i++){
+            int width = all_next[i] - all_prev[i] - 1;
+            long long area = heights[i]*width;
+            if(area > ans){
+                ans = area;
+            }
         }
 
         return ans;
-
     }
 };
