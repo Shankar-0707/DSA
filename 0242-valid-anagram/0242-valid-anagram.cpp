7278230f0293dfdc4f34859632112f0489470dbd
Shankar Jangid <shankarjangid550@gmail.com>
2026-07-02 13:45:53 +0530

    Time: 3 ms (46.3%), Space: 9.8 MB (25.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,9 +3,20 @@ public:
     bool isAnagram(string s, string t) {
         // method 1 - using unordered_map TC - O(n) Space - O(n)
         // method 2 - sort and compare 
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        // mthod 3 - only 26 character will bne there so instead of making hashmap make a frequency array
 
-        return s == t;
+        vector<int> freq(26, 0);
+
+        for(auto i : s){
+            freq[i - 'a']++;
+        }
+        for(auto i : t){
+            freq[i-'a']--;
+        }
+
+        for(auto i : freq){
+            if(i != 0) return false;
+        }
+        return true;
     }
 };
