39c5f5d45dbfc5abd7eeaef0d37af49107fd0081
Shankar Jangid <shankarjangid550@gmail.com>
2026-07-04 15:21:18 +0530

    Time: 0 ms (100%), Space: 10.9 MB (33.8%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -1,54 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
+    vector<int> ans;
+
+    void helper(TreeNode* root){
+        if(!root) return;
+
+        helper(root->left);
+        ans.push_back(root->val);
+        helper(root->right);
+    }
     vector<int> inorderTraversal(TreeNode* root) {
-        vector<int> ans;
-        TreeNode* curr = root;
-
-        while(curr){
-            // jb tk curr node h chlte rho 
-            if(!curr->left){
-                // agar curr ka left nhih mtlb vhi left node h use process kro yani answer me adaldo 
-                ans.push_back(curr->val);
-                curr = curr->right;
-            }
-            else{
-                // mtlb ki uski left node h to iska mtlb h curr kisi na kisi ka predecessor hoga 
-                TreeNode* leftChild = curr->left;
-
-                while(leftChild->right && leftChild->right != curr){
-                    leftChild = leftChild->right;
-                }
-                if(!leftChild->right){
-                    leftChild->right = curr;
-                    curr = curr->left;
-                }
-                else{
-                    // mtlb vo already visited h cycle bn jaegi 
-                    leftChild->right = NULL;
-                    ans.push_back(curr->val);
-                    curr = curr->right;
-                }
-
-
-                // ab hme vo noe mil gyi h jiska predecessor curr hoga 
-                // leftChild->right = curr;
-                // TreeNode* temp = curr;
-                // curr = curr->left;
-                // temp->left = NULL;
-            }
-        }
-
+        helper(root);
         return ans;
     }
 };
