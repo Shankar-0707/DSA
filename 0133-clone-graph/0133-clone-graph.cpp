99e2f59747a2db87dbe60580fb3c6ad09af8ed1d
Shankar Jangid <shankarjangid550@gmail.com>
2026-07-17 00:01:34 +0530

    Time: 7 ms (22.37%), Space: 11.9 MB (81.36%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -23,6 +23,8 @@ class Solution {
 public:
     unordered_map<Node*, Node*> mp; // jo ye btayega ki kis node ke liye konsi cloned_node bni hui h 
 
+    queue<Node*> q;
+
     void dfs(Node* node, Node* cloned_node){
         for(auto n : node->neighbors){
             if(mp.find(n) == mp.end()){
@@ -39,13 +41,40 @@ public:
         }
     }
 
+    void bfs(Node* node){
+     
+
+        while(!q.empty()){
+            auto n = q.front();
+            q.pop();
+
+            Node* cloned_node = mp[n];
+
+            for(auto i : n->neighbors){
+                if(mp.find(i) == mp.end()){
+                    // iska mtlb clone kro 
+                    Node* clone = new Node(i->val);
+                    mp[i] = clone;
+                    cloned_node->neighbors.push_back(clone);
+                    q.push(i);
+                }
+                else{
+                    cloned_node->neighbors.push_back(mp[i]);
+                }
+            }
+        }
+    }
+
     Node* cloneGraph(Node* node) {
         if(!node) return NULL;
         mp.clear();
+        
         Node* cloned_node = new Node(node->val);
         mp[node] = cloned_node;
+        q.push(node);
 
-        dfs(node, cloned_node);
+        // dfs(node, cloned_node);
+        bfs(node);
         return cloned_node;
     }
 };
