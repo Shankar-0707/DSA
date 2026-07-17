49d1b2aab859a75f14bb3d83909f34e44ca410ca
Shankar Jangid <shankarjangid550@gmail.com>
2026-07-18 02:23:42 +0530

    Time: 0 ms (100%), Space: 19.4 MB (72.5%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
-    unordered_map<int,bool> visited;
-    unordered_map<int, vector<int>> adjList;
-    int ans = 0;
 
-    void dfs(int n){
-        visited[n] = true;
 
-        for(auto nbr : adjList[n]){
-            if(!visited[nbr]){
-                dfs(nbr);
+    void dfs(int node, vector<vector<int>>& isConnected, vector<bool>& visited){
+        visited[node] = 1;
+
+        for(int j=0; j<isConnected.size(); j++){
+            if(isConnected[node][j] && !visited[j]){
+                dfs(j, isConnected, visited);
             }
         }
     }
 
     int findCircleNum(vector<vector<int>>& isConnected) {
-        // Simple ye btana h ki kitne disconnected components h 
         int n = isConnected.size();
 
+        vector<bool> visited(n, 0);
+        int ans = 0;
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(i == j) continue;
-                else{
-                    if(isConnected[i][j] == 1){
-                        adjList[i+1].push_back(j+1);
-                    }
-                }
-            }
-        }
-        // We have created adjList list now track the ans by any search bfs or dfs
-        for(int i=1; i<=n; i++){
             if(!visited[i]){
+                // agar ith node viisited nhi h to phle uske sare nneigbours per chle jao 
                 ans++;
-                dfs(i);
+                dfs(i, isConnected, visited);
             }
         }
+
         return ans;
     }
 };
