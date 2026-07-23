4c3bc707a78ce3dca621934a6e13871df00167a2
Shankar Jangid <shankarjangid550@gmail.com>
2026-07-23 15:39:49 +0530

    Time: 0 ms (100%), Space: 198.8 MB (60.63%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
@@ -3,9 +3,13 @@ public:
     int uniqueXorTriplets(vector<int>& nums) {
         int n = nums.size();
         if(n == 1 || n == 2) return n;
+
         int ans = 1;
+
         while(ans <= n){
             ans*=2;
+
+            // n ki kisi bhi balue ki highest answer 2 ki power uske nearest big one tk hi ja skta h 
         }
 
         return ans;
