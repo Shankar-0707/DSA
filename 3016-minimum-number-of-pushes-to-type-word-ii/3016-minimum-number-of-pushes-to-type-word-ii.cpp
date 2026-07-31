379b778898d0f979561b626c2751bd115f90c743
Shankar Jangid <shankarjangid550@gmail.com>
2026-07-31 12:09:54 +0530

    Time: 0 ms (100%), Space: 25.4 MB (72.47%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n = word.length();
+        vector<int> freq(26, 0);
+        for(auto i : word){
+            freq[i-'a']++;
+        }
+
+        sort(freq.begin(), freq.end());
+        reverse(freq.begin(), freq.end());;
+        unordered_map<int,int> mp; // ye btayeega ki ye character minimum kitne pushes pe mapped h 
+        int ans = 0;
+        int push = 1;
+        for(int i=0; i<freq.size(); i++){
+            if(i != 0 && i % 8 == 0){
+                push++;
+            }
+
+            ans+= freq[i] * push;
+
+        }
+
+        return ans;
+    }
+};
