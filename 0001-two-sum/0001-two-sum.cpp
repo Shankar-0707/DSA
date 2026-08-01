0e01b197f07aea91e1cbb3e7e7a2d849082cba63
Shankar Jangid <shankarjangid550@gmail.com>
2026-08-01 17:00:44 +0530

    Time: 9 ms (37.8%), Space: 18.2 MB (5.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,13 +2,40 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-        // Brute force Approach - 1 Nested Loop - O(n^2)
+        unordered_map<int, vector<int>> indexes; // ek number ke mulitple indexes ho skte h 
+
         for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                if(nums[i] + nums[j] == target) return {i, j};
+            indexes[nums[i]].push_back(i);
+        }
+
+        // sort krenge and then apply krenge two pointers 
+        sort(nums.begin(), nums.end());
+
+        int a = 0;
+        int b = n-1;
+
+        while(a < b){
+            // a <= b isiliye nhi rkha kyoki a == b pe ek hi element ko twice time include krlete jo ki nhi kr skte 
+
+            int sum = nums[a] + nums[b];
+            if(sum == target){
+                // return krna h khuch
+                if(nums[a] != nums[b]){
+                    // yani ki dono hi value alag alag h to simple dono ke indexes return krdo 
+                    return {indexes[nums[a]][0], indexes[nums[b]][0]};
+                } 
+                else{
+                    return {indexes[nums[a]][0], indexes[nums[b]][1]};
+                }
+            }
+            else if(sum < target){
+                a++;
+            }
+            else{
+                b--;
             }
         }
 
-        return {-1, -1};
+        return {-1,-1};
     }
 };
