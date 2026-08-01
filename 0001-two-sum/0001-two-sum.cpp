c9da2fab53e00d968e78296afa879483f7017f9d
Shankar Jangid <shankarjangid550@gmail.com>
2026-08-01 17:56:56 +0530

    Time: 3 ms (66.36%), Space: 14.9 MB (39.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,26 +2,20 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-        unordered_map<int, vector<int>> indexes; // ek number ke mulitple indexes ho skte h 
-
-        for(int i=0; i<n; i++){
-            indexes[nums[i]].push_back(i);
-        }
-
-        // rather then sorting hm ek linear loop chalyenge or hr ek elemnet milne per check krenge ky atarget - that element present h hmare map me or aagar h to uska index agar is element ke index ke equal nhi hua to vhi return krende
+        unordered_map<int,int> indexes;
 
         for(int i=0; i<n; i++){
             int elem = nums[i];
+
             if(indexes.find(target-elem) != indexes.end()){
-                // iska mtlb h ki vo element present h 
-                for(auto idx : indexes[target-elem]){
-                    if(idx != i){
-                        return {i, idx};
-                    }
-                }
+                // mtlb is ke liye phle ek index aa chuka h asa 
+                return {indexes[target-elem], i};
+                // isse i kbhi us elemnet ke index ke equal bhi nhi hoga so same element twice nhi aaenge 
+            }
+            else{
+                indexes[nums[i]] = i;
             }
         }
-
         return {-1, -1};
     }
 };
