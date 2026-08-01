c18d04dbd999cd77a5794d0cb83a71a63fcf755c
Shankar Jangid <shankarjangid550@gmail.com>
2026-08-01 15:31:09 +0530

    Time: 169 ms (5.37%), Space: 9.5 MB (96.48%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool solve(vector<int>& nums, int score1, int score2, int i, int j,
+               bool turn) {
+        if (i > j) {
+            if (score1 >= score2)
+                return true;
+
+            return false;
+        }
+
+        if (turn) {
+            // player1 choose krega do option h uske paas
+
+            bool one = solve(nums, score1 + nums[i], score2, i + 1, j, !turn);
+            bool two = solve(nums, score1 + nums[j], score2, i, j - 1, !turn);
+
+            return one || two;
+        }
+
+        bool one = solve(nums, score1 , score2 + nums[i], i + 1, j, !turn);
+        bool two = solve(nums, score1 , score2 + nums[j], i, j - 1, !turn);
+
+        return one && two;
+    }
+
+    bool predictTheWinner(vector<int>& nums) {
+        int score1 = 0;
+        int score2 = 0;
+
+        bool turn = 1; // 1 -> player1 , 0 -> player2
+
+        int n = nums.size();
+        int i = 0;
+        int j = n - 1;
+
+        return solve(nums, score1, score2, i, j, turn);
+    }
+};
