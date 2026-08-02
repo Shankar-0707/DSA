eb3b200c15fb6e38a65274aed11bf7a3af6751c8
Shankar Jangid <shankarjangid550@gmail.com>
2026-08-02 16:22:46 +0530

    Time: 0 ms (100%), Space: 17.8 MB (60.88%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -2,22 +2,19 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
-
-        // dpMax[i] = ye btayega ki is index per end hone wla abse bda product kya h 
-        // dpMin[i] = ye btayega ki is index per end hone wla sbse chota min product kya h 
-        vector<int> dpMax(n);
-        vector<int> dpMin(n);
-
-        dpMax[0] = nums[0];
-        dpMin[0] = nums[0];
-
         int ans = nums[0];
+        int maxi = nums[0];
+        int mini = nums[0];
 
         for(int i=1; i<n; i++){
-            dpMax[i] = max({nums[i], nums[i]*dpMax[i-1], nums[i]*dpMin[i-1]});
-            dpMin[i] = min({nums[i], nums[i]*dpMax[i-1], nums[i]*dpMin[i-1]});
+            if(nums[i] < 0){
+                swap(maxi, mini);
+            }
 
-            ans = max(ans, dpMax[i]);
+            maxi = max(nums[i], nums[i]*maxi);
+            mini = min(nums[i], nums[i]*mini);
+
+            ans = max(ans, maxi);
         }
 
         return ans;
