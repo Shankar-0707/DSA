27ff9ea6bffef26d9b985154bdee3cb1502cc284
Shankar Jangid <shankarjangid550@gmail.com>
2026-08-02 12:37:02 +0530

    Time: 92 ms (21.53%), Space: 9.6 MB (85.04%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,39 +1,17 @@
 class Solution {
 public:
-    bool solve(vector<int>& nums, int score1, int score2, int i, int j,
-               bool turn) {
-        if (i > j) {
-            if (score1 >= score2)
-                return true;
 
-            return false;
-        }
+    int solve(vector<int>& nums, int i, int j){
+        if(i == j) return nums[i];
 
-        if (turn) {
-            // player1 choose krega do option h uske paas
-
-            bool one = solve(nums, score1 + nums[i], score2, i + 1, j, !turn);
-            bool two = solve(nums, score1 + nums[j], score2, i, j - 1, !turn);
-
-            return one || two;
-        }
-
-        bool one = solve(nums, score1 , score2 + nums[i], i + 1, j, !turn);
-        bool two = solve(nums, score1 , score2 + nums[j], i, j - 1, !turn);
-
-        return one && two; // uyha per && isiliye kyoki player2 agar ek me bhi jeet gya to vo vhi move choose krega obv isiliye player1 ko jitne ke liye player2 ko dono move me harnaa pdega 
+        return max((nums[i] - solve(nums, i+1, j)), (nums[j] - solve(nums, i, j-1)));
     }
 
     bool predictTheWinner(vector<int>& nums) {
-        int score1 = 0;
-        int score2 = 0;
-
-        bool turn = 1; // 1 -> player1 , 0 -> player2
-
         int n = nums.size();
-        int i = 0;
-        int j = n - 1;
+        int i=0; 
+        int j=n-1;
 
-        return solve(nums, score1, score2, i, j, turn);
+        return solve(nums, i, j) >= 0;
     }
 };
