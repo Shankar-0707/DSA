de571924da290d2fc71abbfc78a3d88821480e10
Shankar Jangid <shankarjangid550@gmail.com>
2026-08-06 14:40:35 +0530

    Time: 0 ms (100%), Space: 8.2 MB (49.31%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,10 +3,10 @@ public:
     int hammingWeight(int n) {
         int ans = 0;
         while(n){
-            int bit = n & 1;
+            int bit = n & 1; // As this operator tells us that the number if 1 then its and with 1 is also set 
             if(bit) ans++;
 
-            n = n >> 1;
+            n = n >> 1; // so that we can go to next bit and caluclate whether i is set or not 
         }
 
         return ans;
