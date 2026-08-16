1fc1d75f430ff54912c79c89a32bb184cfe0d2f5
Shankar Jangid <shankarjangid550@gmail.com>
2026-08-16 15:51:32 +0530

    Time: 0 ms (100%), Space: 7.9 MB (35.76%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        // This requires Bit manipulations using XOR and carry approach 
-        // basiclaly any sum is equal to -> sum by XOR without carry and Sum of carry 
-
-        while(b != 0){
-            int sum = a ^ b;
-            int carry = (a & b ) << 1; // as this carry goes to next bit for further calculation
-            b = carry;
+        // sum = sum wihtout carry + carry 
+        while(b!= 0){
+            int sum = (a ^ b);
+            int carry = (a & b) << 1;
             a = sum;
+            b = carry;
         }
 
         return a;
