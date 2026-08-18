e17ddce49f5c0dc38cab65eeebaa97721fd77a0d
Shankar Jangid <shankarjangid550@gmail.com>
2026-08-18 15:19:13 +0530

    Time: 417 ms (26.56%), Space: 41.8 MB (34.36%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,49 +1,31 @@
 class Solution {
 public:
-    // int solve(int index, int prev_index, vector<int>& nums,
-    // vector<vector<int>> &dp){
-    //     if(index >= nums.size()) return 0;
-
-    //     if(dp[index][prev_index+1] != -1) return dp[index][prev_index+1];
-    //     int len = 0;
-
-    //     // include
-    //     if(prev_index == -1 || nums[index] > nums[prev_index]){
-    //         len = max(1 + solve(index+1, index, nums, dp), len);
-    //     }
-
-    //     len = max(solve(index+1, prev_index, nums, dp), len);
-
-    //     dp[index][prev_index+1] = len;
-    //     return len;
-
-    // }
-
-    int solve(int index, int prev_index, vector<int>& nums) {
-        int n = nums.size();
-
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
-
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = i - 1; j >= -1; j--) {
-                int inc = 0;
-                if (j == -1 || nums[i] > nums[j]) {
-                    inc = 1 + dp[i + 1][i + 1];
-                }
-                int exc = dp[i + 1][j + 1];
-                dp[i][j + 1] = max(inc, exc);
-            }
+    int t[2501][2501];
+    int solve(vector<int>& nums, int i, int prev){
+        if( i >= nums.size()){
+            return 0;
         }
 
-        return dp[0][0];
+        if(t[i][prev+1] != -1) return t[i][prev+1];
+
+        int inc = 0;
+        // include
+        if(prev == -1 || nums[i] > nums[prev]){
+            inc = 1 + solve(nums, i+1, i);
+        }
+
+        // exclude
+        int exc = solve(nums, i+1, prev);
+
+        return t[i][prev+1] = max(inc, exc);
     }
 
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        int prev_index = -1;
-        // vector<vector<int>> dp(n, vector<int>(n+1, -1));
+        // we have to maintain two states one is for curr index and other is for prev index 
 
-        // return solve(0, prev_index, nums, dp);
-        return solve(0, prev_index, nums);
+        int n = nums.size(); 
+        int prev = -1;
+        memset(t, -1, sizeof(t));
+        return solve(nums, 0, prev);
     }
 };
